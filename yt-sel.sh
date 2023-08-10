2ebba6691547b5c91e3e321cfcdaed021346129b
xcr0061 <xcr0061@gmail.com>
2023-08-10 21:17:55 +0400

    Update and cleanup

--- yt-sel.sh
+++ yt-sel.sh
@@ -2,18 +2,13 @@
 
 ## Refer to YT-DLP wiki page for more information. https://github.com/yt-dlp/yt-dlp/wiki
 
-#########################################
-#       YT-DLP - DOWNLOAD QUALITY       #
-#           SELECTIONS                  #
-#                                       #
-#########################################
-#           by XCR0061                  #
-#########################################
-
-set -o errexit
-
-## Uncomment For debugging
-#set -o xtrace
+##############################################
+#       YT-DLP - DOWNLOAD QUALITY            #
+#           SELECTIONS                       #
+#                                            #
+##############################################
+#           by XCR0061                       #
+##############################################
 
 clear
 tput setaf 208
@@ -30,13 +25,37 @@ echo "  ##################################### "
 echo ""
 tput sgr0
 
-## Request URl and check if valid
+##############################################
+
+set -o errexit
+
+## Uncomment For debugging
+#set -o xtrace
+
+## Set Video output Directory (Refer to yt-dlp documentation for setting default output Directory)
+outputdir="$HOME/Videos"
+
+## Set Audio output Directory
+audiodir="$HOME/Music/01_YT-DLP-mp3"
+
+## Check if Directories exist
+if [ ! -d "$outputdir" ] || [ ! -d "$audiodir" ]; then
+
+    echo -e "\nPlease Update or Create Directories"
+    echo -e "-- Either Directory \"$outputdir\" or \"$audiodir\" does not exist --"
+    echo
+    exit 1
+fi
+
+## Request URL and check if valid
 read -p "$(tput setaf 6) $(tput bold)Please Enter URL: $(tput sgr0)" URL
 echo
-if [[ $URL =~ ^https?:// ]]
-then
+
+if [[ $URL =~ ^https?:// ]] ; then
+
     echo "MENU:"
 else
+
     echo "Invalid URL"
     echo
     exit 1
@@ -52,63 +71,62 @@ do
 
         ## Option 1
         "Best available Quality - Full Video")
+
             ## Download Video & Audio then merge
             echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nDownloading Full Video:$(tput rmul) $(tput sgr0)"
-            yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio" --merge-output-format mp4 "$URL"
-            echo
 
-            ## Print done
+            yt-dlp -f "bv[ext=mp4]+ba[ext=m4a]/b" --merge-output-format mp4 -o "$outputdir/%(title)s.%(ext)s" "$URL"
+
             echo -e "$(tput setaf 6) $(tput bold)\nAll Done$(tput sgr0)"
+            echo
             break
             ;;
 
         ## Option 2
         "1080p HD - Full Video")
+
             ## Download Video & Audio then merge
             echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nDownloading 1080p:$(tput rmul) $(tput sgr0)"
-            yt-dlp -S res:1080,ext:mp4:m4a --merge-output-format mp4 "$URL"
-            echo
 
-            ## Print done
+            yt-dlp -S res:1080,ext:mp4:m4a --merge-output-format mp4 -o "$outputdir/%(title)s.%(ext)s" "$URL"
+
             echo -e "$(tput setaf 6) $(tput bold)\nAll Done$(tput sgr0)"
+            echo
             break
             ;;
 
         ## Option 3
         "Audio Only - 192kbps MP3 Output")
+
             ## Download the m4a file
             echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nDownloading Audio only:$(tput rmul) $(tput sgr0)"
-            yt-dlp -f 'bestaudio[ext=m4a]' "$URL"
+
+            yt-dlp -f 'ba[ext=m4a]' -o "$outputdir/%(title)s.%(ext)s" "$URL"
 
             ## Get the downloaded filename & convert to MP3
-            filename="$(yt-dlp --get-filename -f 'bestaudio[ext=m4a]' "$URL")"
-            ffmpeg -i "$filename" -b:a 192k "${filename%.m4a}.mp3"
+            inputfile="$(yt-dlp --get-filename -f 'bestaudio[ext=m4a]' -o "$outputdir/%(title)s.%(ext)s" "$URL")"
+
+            ffmpeg -i "$inputfile" -b:a 192k "${inputfile%.m4a}.mp3"
+            echo
 
             ## Check if the conversion was successful
             if [ $? -eq 0 ] ; then
-                ## MP3 File name clean-up and move to Music folder
-                echo -e "$(tput bold) $(tput smul)\nCleaning up and Moving to Music Folder: /Music/YT-DLP-mp3$(tput rmul) $(tput sgr0)"
 
-                ## Remove space, brackets, and the hash from mp3 file
-                filename2=$(ls *.mp3)
-                clean_filename="${filename2%% \[*}.mp3"
+                ## Delete downloaded file and move MP3 to Music folder
+                echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nCleaning up and Moving MP3 file to Folder: \"$audiodir\"$(tput rmul) $(tput sgr0)"
                 echo
+                outputfile="${inputfile%.m4a}.mp3"
 
-                rm -v "$filename"
-                echo
+                rm -v "$inputfile"
+                mv -v "$outputfile" "$audiodir/"
 
-                if [ -d "$HOME/Music/YT-DLP-mp3" ] ; then
-                    mv -v "$filename2" "$HOME/Music/YT-DLP-mp3/$clean_filename"
-                    echo
-
-                    ## Print done
-                    echo -e "$(tput setaf 6) $(tput bold)\nAll Done$(tput sgr0)"
-                else
-                    echo "\nUnable to move mp3 file: Directory /Music/YT-DLP-mp3 does not exist"
-                    exit 1
-                fi
+                echo -e "$(tput setaf 6) $(tput bold)\nAll Done$(tput sgr0)"
             else
+
+                echo
                 echo "Conversion failed. Original file retained."
+                echo
+                exit 1
             fi
 
             echo
